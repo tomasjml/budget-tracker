e7748ffc2994105cc2129bf629fd9c180904c3a1
Tomás Molina <jtml.mass@gmail.com>
2021-03-15 01:32:32 -0400

    Editing Expense Total to use AppContext

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const ExpenseTotal = () => {
+    const { expenses } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
     return (
         <div className="alert alert-primary">
-            <span>Spent so far: $1000</span>
+            <span>Spent so far: ${totalExpenses}</span>
         </div>
     );
 };
