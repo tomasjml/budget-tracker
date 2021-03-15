620a31f7527d9160d58c319c29340127f1f43ad9
Tomás Molina <jtml.mass@gmail.com>
2021-03-15 01:36:16 -0400

    Editing Remaining to use AppContext

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Remaining = () => {
+    const {expenses, budget} = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
     return (
-        <div className="alert alert-success">
-            <span>Remaining: $1000</span>
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: ${budget - totalExpenses}</span>
         </div>
     );
 };
