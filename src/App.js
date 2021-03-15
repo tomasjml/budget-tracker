dd64b0ab72c4d52de2ade71368912c405486a51b
Tomás Molina <jtml.mass@gmail.com>
2021-03-15 01:53:49 -0400

    Adding content into Card

--- src/App.js
+++ src/App.js
@@ -11,31 +11,39 @@ const App = () => {
   return (
     <AppProvider>
     <div className='container'>
-      <h1 className='mt-3'>My Budget Planner</h1>
-      <div>
-        <div className='row mt-3'>
-          <div className='col-sm'>
-            <Budget />
-          </div>
-          <div className='col-sm'>
-            <Remaining />
-          </div>
-          <div className='col-sm'>
-            <ExpenseTotal />
-          </div>
-        </div>
-      </div>
-      <div>
-        <h3 className='mt-3'>Expenses</h3>
-        <div className='row mt-3'>
-          <div className='col-sm'>
-            <ExpenseList />
-          </div>
-        </div>
-        <h3 className='mt-3'>Add Expense</h3>
-        <div className='row mt-3'>
-          <div className='col-sm'>
-            <AddExpenseForm />
+      <br></br>
+      <br></br>
+      <div className='card align-self-center'>
+        <div className='card-header'>
+          <h1 className='card-title mt-3'>My Budget Planner</h1>
+          <div>
+            <div className='row mt-3'>
+              <div className='col-sm'>
+                <Budget />
+              </div>
+              <div className='col-sm'>
+                <Remaining />
+              </div>
+              <div className='col-sm'>
+                <ExpenseTotal />
+              </div>
+            </div>
+            <div className='card-body'>
+            <div>
+              <h3 className='card-card-subtitle mt-3'>Expenses</h3>
+              <div className='row mt-3'>
+                <div className='col-sm'>
+                  <ExpenseList />
+                </div>
+              </div>
+              <h3 className='card-card-subtitle mt-3'>Add Expense</h3>
+              <div className='row mt-3'>
+                <div className='col-sm'>
+                  <AddExpenseForm />
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
