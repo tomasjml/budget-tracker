53240909cde18e50a3a80af2cc185fd171a4d789
Tomás Molina <jtml.mass@gmail.com>
2021-03-15 00:28:25 -0400

    Create Expense Total Component

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExpenseTotal = () => {
+    return (
+        <div className="alert alert-primary">
+            <span>Spent so far: $1000</span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
