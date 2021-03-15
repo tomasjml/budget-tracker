761ef7ab3799b8c123fb2e7a10534aa7c3e9e9ec
Tomás Molina <jtml.mass@gmail.com>
2021-03-15 00:55:54 -0400

    Adding Expense form component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -12,7 +12,7 @@ const ExpenseList = () => {
         <ul className='list-group'>
             {expenses.map((expense) => (
                 <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost}/>
-            ))};
+            ))}
         </ul>
     );
 };
